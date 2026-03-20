1a7fdf50172c37783aaed5c7be725f13de4edd56
Oleg Borodkin <82386871+a374ru@users.noreply.github.com>
2026-03-20 13:34:40 +0300

    Тестирование тега

--- srvr/serv.go
+++ srvr/serv.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>XXXXXXXXXXX: %s из папки: <span style='color:red'>%s</span></h1>", Varr(r.URL.Path), r.URL.Host)
+	fmt.Fprintf(w, "<h1> тестирование тега: %s из папки: <span style='color:red'>%s</span></h1>", Varr(r.URL.Path), r.URL.Host)
 }
 
 func Serv() {
